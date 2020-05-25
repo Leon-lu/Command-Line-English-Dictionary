f6c25afeb052ce81046460f0003c7910c2694b50
null null <salsinan@protonmail.com>
2020-05-25 00:51:18 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import json
+import difflib
+
+
+data = json.load(open("data.json"))
+
+def translate(w):
+  w = w.lower()
+  if w in data:
+    return data[w]
+  elif w.upper() in data:
+    return data[w.upper()]
+  elif w.title() in data:
+    return data[w.title()]
+
+  close_words = difflib.get_close_matches(w, data, cutoff=0.7)
+
+  if len(close_words) > 0:
+    yn = input("Did you mean: {}? Y/N: ".format(close_words[0]))
+
+    if yn.lower() == "y":
+      return data[close_words[0]]
+    
+  return "Word not found. Please check your spelling and try again."
+
+word = ""
+while word.lower() != "q":
+  word = input("Please enter a word (or q to quit): ")
+  
+  if word != "q":
+    meanings = translate(word)
+    if type(meanings) == str:
+      print(meanings)
+    else:
+      for meaning in meanings:
+        print("\n" + meaning)
